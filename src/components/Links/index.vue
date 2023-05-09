32eef99a0e0c9c9edce53c8d59d4731c76358cae
m0rml1n <1233344@qq.com>
2023-05-09 21:45:36 +0800

    Update index.vue

--- src/components/Links/index.vue
+++ src/components/Links/index.vue
@@ -46,32 +46,32 @@ let linksData = [
   {
     icon: Blog,
     name: "博客",
-    link: "https://blog.imsyy.top/",
+    link: "https://nai.si/",
   },
   {
     icon: Cloud,
     name: "网盘",
-    link: "https://pan.imsyy.top/",
+    link: "https://oss.nai.si/",
   },
-  {
-    icon: CompactDisc,
-    name: "音乐",
-    link: "https://music.imsyy.top/",
+    {
+    icon: Book,
+    name: "书签",
+    link: "https://yyds.page/",
   },
   {
     icon: Compass,
-    name: "起始页",
-    link: "https://nav.imsyy.top/",
+    name: "空白页",
+    link: "about:blank",
   },
-  {
-    icon: Book,
-    name: "网址集",
-    link: "https://web.imsyy.top/",
+ {
+    icon: CompactDisc,
+    name: "空白页",
+    link: "about:blank",
   },
   {
     icon: Fire,
-    name: "今日热榜",
-    link: "https://hot.imsyy.top/",
+    name: "空白页",
+    link: "about:blank",
   },
 ];
 
